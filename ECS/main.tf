31a8ec48f7deba3fa267711f7c3d9cac95aebc2f
davidsmenesesc <109972931+davidsmenesesc@users.noreply.github.com>
2022-12-22 14:32:04 -0500

    Networking

--- ECS/main.tf
+++ ECS/main.tf
@@ -1,24 +1,24 @@
 resource "aws_ecr_repository" "my_first_ecr_repo" {
   name = "dm-repo-rampup"
 }
-# resource "aws_ecs_cluster" "my_cluster" {
-#   name = "dm-cluster" # Naming the cluster
-# }
-# resource "aws_iam_role" "eScsTaskExecutionRole" {
-#   name               = "ecsTaskExecutionRole"
-#   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-# }
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "dm-cluster" # Naming the cluster
+}
+resource "aws_iam_role" "eScsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
 
-# data "aws_iam_policy_document" "assume_role_policy" {
-#   statement {
-#     actions = ["sts:AssumeRole"]
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
 
-#     principals {
-#       type        = "Service"
-#       identifiers = ["ecs-tasks.amazonaws.com"]
-#     }
-#   }
-# }
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
 
 # resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
 #   role       = aws_iam_role.ecsTaskExecutionRole.name
