eb22347251bbf7a821cefae526ae5ebe033d806f
davidsmenesesc <109972931+davidsmenesesc@users.noreply.github.com>
2022-12-22 14:28:13 -0500

    Networking

--- Network/variables.tf
+++ Network/variables.tf
@@ -7,15 +7,15 @@ variable "vpc_cidr" {
 variable "subnet_vpc_cidr_a" {
   description = "CIDR range of VPC subnets"
   type        = string
-  default     = "10.1.0.0/16"
+  default     = "10.1.0.0/24"
 }
 variable "subnet_vpc_cidr_b" {
   description = "CIDR range of VPC subnets"
   type        = string
-  default     = "10.1.1.0/16"
+  default     = "10.1.1.0/24"
 }
 variable "subnet_vpc_cidr_c" {
   description = "CIDR range of VPC subnets"
   type        = string
-  default     = "10.1.2.0/16"
+  default     = "10.1.2.0/24"
 }
